9d1b864e77003151a07ac3b76eb1f02cb6f1507d
piriottu <nicola.piriottu@abinsula.com>
2023-06-05 14:49:58 +0200

    Initial commit

---
+++ lib/app.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:news_app_flutter/pages/my_hello_world_page.dart';
+
+class App extends StatelessWidget{
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // TRY THIS: Try running your application with "flutter run". You'll see
+        // the application has a blue toolbar. Then, without quitting the app,
+        // try changing the seedColor in the colorScheme below to Colors.green
+        // and then invoke "hot reload" (save your changes or press the "hot
+        // reload" button in a Flutter-supported IDE, or press "r" if you used
+        // the command line to start the app).
+        //
+        // Notice that the counter didn't reset back to zero; the application
+        // state is not lost during the reload. To reset the state, use hot
+        // restart instead.
+        //
+        // This works for code too, not just values: Most code changes can be
+        // tested with just a hot reload.
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: MyHelloWorldPage(),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:news_app_flutter/app.dart';
+
+void main() {
+  runApp(App());
+}
